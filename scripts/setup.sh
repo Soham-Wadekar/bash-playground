cca3bd9b06f8ff183c469d9e785b38c7eb5886b2
Soham Wadekar <sohamw03@gmail.com>
2025-10-22 19:47:41 +0530

    add error handling

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,3 +6,5 @@ source ./config/playground.env
 
 mkdir -p $PROJECT_HOME
 mkdir -p $LOG_DIR $PID_DIR
+
+chmod +x ./services/*
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -7,7 +7,7 @@ SERVICES=$(ls ./services/)
 for service in ${SERVICES[@]}; do
     if [ -f $PID_DIR/$service.pid ]; then
         PID=$(cat $PID_DIR/$service.pid)
-        kill $PID
+        kill $PID 2>/dev/null
         echo "[$PID] Stopped $service service"
         rm $PID_DIR/$service.pid
     fi
