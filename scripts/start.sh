0c2a5e71202ef14ac0752203ff275a785570764f
Soham Wadekar <sohamw03@gmail.com>
2025-10-22 17:40:11 +0530

    add log generator service

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,3 +5,7 @@ source ./config/playground.env
 ./services/heartbeat "$LOG_DIR/heartbeat.log" &
 echo "[$!] Starting heartbeat service"
 echo $! > $PID_DIR/heartbeat.pid
+
+./services/log_generator "$LOG_DIR/generator.log" &
+echo "[$!] Starting log generator service"
+echo $! > $PID_DIR/generator.pid
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -8,3 +8,10 @@ if [ -f $PID_DIR/heartbeat.pid ]; then
     echo "[$PID] Stopped heartbeat service"
     rm $PID_DIR/heartbeat.pid
 fi  
+
+if [ -f $PID_DIR/generator.pid ]; then
+    PID=$(cat $PID_DIR/generator.pid)
+    kill $PID
+     echo "[$PID] Stopped log generator service"
+    rm $PID_DIR/generator.pid
+fi
